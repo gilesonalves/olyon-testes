66a0c8527e6bb0a7c0c4023b4851268c09a57edd
Jhonnatan Soares Rebuli <jhonnatansr@gmail.com>
2025-11-28 18:18:06 -0300

    feat: add sidebar

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import SidebarLayout from "@/components/sidebarLayout"
+ 
+export default function Layout() {
+  return (
+    <SidebarLayout>
+      <div>conteúdo</div>
+    </SidebarLayout>
+  )
+}
