e958c2858e96a3aee3a1d8ce1f0e1ab7e5454ade
gilesonalves <gilesonalves@gmail.com>
2025-12-01 17:10:23 -0300

    pagina cadastro form

--- app/recuperar-senha/page.tsx
+++ app/recuperar-senha/page.tsx
@@ -1,5 +1,45 @@
+"use client"
+
+import * as React from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Controller, useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "@/components/ui/button"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { toast } from "sonner"
+
+const formSchema = z.object({
+  email: z.string().email("Digite um email válido"),
+
+})
+
 
 export default function RecuperarSenha() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: ""
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof formSchema>) {
+    toast("You submitted the following values:", {
+      description: (
+        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
+          <code>{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+      position: "bottom-right",
+      classNames: {
+        content: "flex flex-col gap-2",
+      },
+      style: {
+        "--border-radius": "calc(var(--radius)  + 4px)",
+      } as React.CSSProperties,
+    })
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-white px-4">
       <div className="w-full max-w-md">
@@ -15,25 +55,35 @@ export default function RecuperarSenha() {
           </h2>
         </div>
 
-        <div>
-          <div className="space-y-2 pb-6">
-            <label className="block text-sm font-medium">
-              Email
-            </label>
-            <input
-              id="email"
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+          <FieldGroup>
+            <Controller
               name="email"
-              type="email"
-              placeholder="Digite aqui seu email"
-              className="w-full border border-gray-400 rounded px-3 py-2"
-              required
-            />
-          </div>
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="email">
+                    Email
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id="email"
+                    type="email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Digite aqui seu Email"
 
-          
-          <button type="submit" className="btn-primary mt-4">Recuperar</button>
-        </div>
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+          </FieldGroup>
+          <Button type="submit" variant={"primary"} className="w-full cursor-pointer mt-6">Recuperar</Button>
+        </form>
       </div>
     </div>
+
   );
 }
