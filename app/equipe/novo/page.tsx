d7ab632f14911a0827d9d9be5405701230e27870
gilesonalves <gilesonalves@gmail.com>
2025-12-03 13:47:21 -0300

    schema

--- app/equipe/novo/page.tsx
+++ app/equipe/novo/page.tsx
@@ -1,45 +1,18 @@
 "use client"
 
-import * as React from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
-import * as z from "zod"
+import { Controller } from "react-hook-form"
 import { Button } from "@/components/ui/button"
 import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-import { toast } from "sonner"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Label } from "@/components/ui/label"
+import Controllers from "../controllers/page"
 
-const formSchema = z.object({
-  name: z.string().min(1, "Nome é obrigatório"),
-
-})
 
 export default function ItemNovo() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      name: ""
-    },
-  })
+  const { form, onSubmit } = Controllers()
 
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    toast("You submitted the following values:", {
-      description: (
-        <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-          <code>{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-      position: "bottom-right",
-      classNames: {
-        content: "flex flex-col gap-2",
-      },
-      style: {
-        "--border-radius": "calc(var(--radius)  + 4px)",
-      } as React.CSSProperties,
-    })
-  }
+  
 
   return (
     <div className="bg-white px-6 py-7">
