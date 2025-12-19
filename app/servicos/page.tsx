f045b9718a6f2124fe66898fa75d8fada871076b
gilesonalves <gilesonalves@gmail.com>
2025-12-19 11:25:01 -0300

    pagina servicos form

--- app/servicos/page.tsx
+++ app/servicos/page.tsx
@@ -1,4 +1,16 @@
-
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 export default function Servicos() {
   return (
     <div className="bg-white px-6 py-7">
@@ -6,7 +18,36 @@ export default function Servicos() {
         <p>
           Serviços
         </p>
-        <button type="submit" className="btn-segundary">Novo</button>
+        {/* <button type="submit" className="btn-segundary">Novo</button> */}
+        <Dialog>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="primary">Novo</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Novo serviço</DialogTitle>
+            
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
       </div>
       <div className="space-y-3">
         <div className="flex flex-col gap-3 rounded-lg border border-gray-200 bg-white px-4 py-3 sm:flex-row sm:items-center sm:justify-between">
