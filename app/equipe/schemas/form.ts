7de70ebb1e28bdc5fc209103ae7c28c745196b21
gilesonalves <gilesonalves@gmail.com>
2026-01-07 10:40:18 -0300

    equipe item novo adicionei formulario

--- app/equipe/schemas/form.ts
+++ app/equipe/schemas/form.ts
@@ -2,7 +2,9 @@ import { z } from "zod"
 
 const FormSchema = z.object({
   name: z.string().min(1, "Nome é obrigatório"),
-  
+  services: z
+    .array(z.string())
+    .min(1, "Selecione pelo menos um serviço"),
 })
 
 export default FormSchema
